e6ae8f94604380c9db267b125519b91307dfdf9c
camilonfs1 <camilonfs1@gmail.com>
2021-02-02 20:09:18 -0500

    express

--- Express/index.js
+++ Express/index.js
@@ -15,25 +15,37 @@ server.listen(3000, ()=>{
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res)=>{
+app.use(express.json());
+
+app.all('/user',(req, res,next)=>{
+    console.log('por aqui paso');
+    next();
+});
+
+app.get('/user',(req, res)=>{
     res.json({
         username:'camilo',
         url:'https://www.youtube.com/watch?v=794Q71KVw1k&ab_channel=Fazt'
     })
-})
+});
 
-app.post('/about',(req, res)=>{
+app.post('/user/:id',(req, res)=>{
+    console.log(req.body);
+    console.log(req.params);
     res.send('peticion post recivida')
-})
+});
 
-app.put('/contact',(req, res)=>{
-    res.send('petcion put recivida')
-})
+app.put('/user/:id',(req, res)=>{
+    console.log(req.body);
+    console.log(req.params);
+    res.send(`Usuario ${req.params.id} actualizado`)
+});
 
-app.delete('/test',(req, res)=>{
-    res.send('peticion delete recivida')
-})
+app.delete('/user/:userid',(req, res)=>{
+    console.log(req.params);
+    res.send(`Usuario ${req.params.userid} eliminado`)
+});
 
 app.listen(3000,()=>{
-    console.log('Server on port 3000');
-})
+    console.log('Server on port 3000')
+});
